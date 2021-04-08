91c7f714a972b858130f246aa0b37d52e4fd78aa
molinajulian245 <molinajulian245@gmail.com>
2021-04-08 17:45:56 -0500

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti expedita sapiente atque aliquid! Illo dolores nemo voluptatem placeat aliquam voluptas provident. Blanditiis iusto impedit totam quisquam qui cumque! Impedit, ipsum!
+        </p>
+        <button class="btn btn-primary">button</button>
+        <br>
+        <i>Andres Molina: KEY</i>
+        </div>
+    </div>
+</div>
+     
+</body>
+</html>
